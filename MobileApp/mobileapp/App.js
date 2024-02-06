45a18f48689b41602caded06a35aef76a74e9489
MRTERRIFIC007 <royom8888@gmail.com>
2024-02-06 10:54:47 +0530

    Included the login page

--- MobileApp/mobileapp/App.js
+++ MobileApp/mobileapp/App.js
@@ -2,14 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import Login from "./Components/Login";
 import MainContainer from "./navigation/MainContainer";
-import OnBoarding from "./Components/OnBoarding";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <OnBoarding />
-      <StatusBar style="auto" />
-    </View>
+   <onBoarding/>
   );
 }
 
--- MobileApp/mobileapp/Components/InputNumber.js
+++ MobileApp/mobileapp/Components/InputNumber.js
@@ -1,24 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { View, TextInput, Button, StyleSheet } from "react-native";
+import { View, TextInput, TouchableOpacity, Text, StyleSheet } from "react-native";
 import { Picker } from "@react-native-community/picker";
 import { AsYouType, parsePhoneNumberFromString } from "libphonenumber-js";
 import { CountryCodePicker, CountryCodeKey } from "./CountryCodes";
 
 const InputNumber = () => {
-  // state for controlled text
   const [text, setText] = useState("");
-  // selected country code
-  const [code, setCode] = useState("101"); // default country code
-  // conditional rendering of TextInput font size
+  const [code, setCode] = useState("101");
   const [textSize, setTextSize] = useState(15);
 
-  // condtion for placeholder to be a smaller font size than the text
   useEffect(() => {
     if (text.length > 0) setTextSize(18);
     else if (text.length === 0) setTextSize(15);
   }, [text]);
 
-  // function for controlled text
   const onTextChange = (number) => {
     const num = parsePhoneNumberFromString(number, CountryCodeKey[code][0]);
     let reg = /^[0-9]/;
@@ -36,7 +31,6 @@ const InputNumber = () => {
     }
   };
 
-  // function that is called when TextInput is submitted
   const signInWithPhone = () => {
     const num = parsePhoneNumberFromString(text, CountryCodeKey[code][0]);
     if (!!num && num.isPossible()) {
@@ -72,7 +66,15 @@ const InputNumber = () => {
           textAlign="left"
         />
       </View>
-      <Button style={styles.button} title="Get OTP" onPress={signInWithPhone} />
+      {/* Use TouchableOpacity for custom-styled button */}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={signInWithPhone}
+      >
+        <Text style={{ color: "#ffffff", fontWeight: "bold", fontSize: 16 }}>
+          Get OTP
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -91,11 +93,12 @@ const styles = StyleSheet.create({
     marginLeft: -15,
   },
   button: {
-    marginTop: 20,
+    marginTop: 3,
     width: 300,
-    backgroundColor: "#016CC6",
+    backgroundColor: "blue",
     padding: 10,
     borderRadius: 5,
+    alignItems: "center",
   },
 });
 
